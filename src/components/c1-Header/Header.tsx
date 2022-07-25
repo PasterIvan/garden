203275c8462a2d42984eccac86efa9ae1ff1c13c
Ivan <paster-ivan@yandex.by>
2022-07-26 02:07:16 +0300

    26.07

--- src/components/c1-Header/Header.tsx
+++ src/components/c1-Header/Header.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import {Contacts} from "./h2-Contacts/Contacts";
-import {MenuWithLogo} from "./h1-Menu/MenuWithLogo";
 import style from "./Header.module.css"
+import {Menu} from "./h1-Menu/Menu";
 
 export const Header = () => {
     return (
-        <div className={style.background}>
-            <div className={style.header}>
-                <div className={style.menu}>
-                    <MenuWithLogo/>
-                    <div className={style.contacts}>
-                        <Contacts/>
-                    </div>
-                </div>
-            </div>
+        <div className={style.flower}>
+            <header>
+                <Menu/>
+                <Contacts/>
+            </header>
         </div>
     )
 }
--- src/components/c2-Body/b1-Main/m3-GardenPlan/GardenPlan.tsx
+++ src/components/c2-Body/b1-Main/m3-GardenPlan/GardenPlan.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import style from "./GardenPlan.module.css"
 
 export const GardenPlan = () => {
     return (
-        <>
-            План сада
-        </>
+        <div className={style.plan}>
+            <div className={style.header}>
+                <div className={style.title}>
+                    ПЛАН САДА
+                </div>
+                <div className={style.text}>
+                    Парк разделен на 2 зоны : открытую, для активного отдыха и закрытую в тени садовых деревьев.
+                </div>
+            </div>
+        </div>
     );
 }
