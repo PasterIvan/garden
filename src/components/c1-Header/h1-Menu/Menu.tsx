69b8b7f1ebaac76bfc2c71407de4e90c01de3690
Ivan <paster-ivan@yandex.by>
2022-07-26 20:05:06 +0300

    Шапка адаптирована готова

--- src/components/c1-Header/h1-Menu/Menu.tsx
+++ src/components/c1-Header/h1-Menu/Menu.tsx
@@ -1,34 +1,34 @@
 import {NavLink} from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
 import Button from "../../../common/Button/Button";
 import style from "./Menu.module.css"
 import logo from "./img/logo.svg";
 
 export const Menu = () => {
+    const [menuActive, setMenuActive] = useState(false)
+
     return (
         <div className={style.menu}>
             <div className={style.logo}>
                 <img src={logo}/>
                 <div>Наш Сад</div>
             </div>
-            <Button className={style.buttonMenu}>меню</Button>
+            <Button className={style.buttonMenu} onClick={() => setMenuActive(!menuActive)}>меню</Button>
             <div className={style.textAndButton}>
-                <div className={style.text}>
+                <div className={`${menuActive ? style.menuActive : style.text}`}>
                     <div>
-                        <NavLink to={'/main'}>наш сад</NavLink>
-                    </div>
-                    <div>
-                        <NavLink to={'/about_us'}>О нас</NavLink>
-                    </div>
-                    <div>
-                        <NavLink to={'/concurs'}>конкурсные работы</NavLink>
-                    </div>
-                    <div>
-                        <NavLink to={'/about_festival'}>О фестивале</NavLink>
-                    </div>
-                </div>
 
-                <Button className={style.button}>деревья</Button>
+                            <NavLink to={'/main'}>наш сад</NavLink>
+
+                            <NavLink to={'/about_us'}>О нас</NavLink>
+
+                            <NavLink to={'/concurs'}>конкурсные работы</NavLink>
+
+                            <NavLink to={'/about_festival'}>О фестивале</NavLink>
+
+                    </div>
+                    <Button className={style.buttonTrees}>деревья</Button>
+                </div>
             </div>
         </div>
     )
