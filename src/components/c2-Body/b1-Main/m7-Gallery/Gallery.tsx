f85bd0be632586f231bcf3e178b579300f5dbcfe
Ivan <paster-ivan@yandex.by>
2022-07-30 01:19:22 +0300

    refactoring

--- src/components/c2-Body/b1-Main/m7-Gallery/Gallery.tsx
+++ src/components/c2-Body/b1-Main/m7-Gallery/Gallery.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./Gallery.module.css"
-import {Photo} from "./Photo/Photo";
+import page1 from "./img/Rectangle 7911.svg";
+import page2 from "./img/Rectangle 7912.svg";
+import page3 from "./img/Rectangle 7913.svg";
 
 export const Gallery = () => {
 
@@ -11,7 +13,12 @@ export const Gallery = () => {
                     title="YouTube video player" frameBorder="0"
                    >
             </iframe>
-            <Photo/>
+            <div className={style.picture}>
+                <img src={page1}/>
+                <img src={page2}/>
+                <img src={page3}/>
+                <img src={page1}/>
+            </div>
         </div>
     );
 }
