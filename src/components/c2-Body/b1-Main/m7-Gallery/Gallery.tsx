6aa046b10069ab7a9d39ecf8e1aa7c4fbc64c9ac
Ivan <paster-ivan@yandex.by>
2022-07-30 00:29:49 +0300

    Gallery без кнопок фотогаллереи с адаптацией

--- src/components/c2-Body/b1-Main/m7-Gallery/Gallery.tsx
+++ src/components/c2-Body/b1-Main/m7-Gallery/Gallery.tsx
@@ -1,32 +1,17 @@
 import React from "react";
 import style from "./Gallery.module.css"
-import page1 from "./img/Rectangle 7911.svg"
-import page2 from "./img/Rectangle 7912.svg"
-import page3 from "./img/Rectangle 7913.svg"
+import {Photo} from "./Photo/Photo";
 
 export const Gallery = () => {
 
     return (
         <div className={style.gallery}>
-            <div className={style.video}>
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/evSn0cGIxfs"
-                        style={{borderRadius: "28px"}}
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen>
-
-                </iframe>
-            </div>
-            <div className={style.picture}>
-                <img src={page1}
-                     className={style.round}/>
-                <img src={page2}
-                     className={style.round}/>
-                <img src={page3}
-                     className={style.round}/>
-                <img src={page1}
-                     className={style.round}/>
-            </div>
+            <iframe width="100%" height="519px" src="https://www.youtube.com/embed/evSn0cGIxfs"
+                    style={{borderRadius: "28px"}}
+                    title="YouTube video player" frameBorder="0"
+                   >
+            </iframe>
+            <Photo/>
         </div>
     );
 }
