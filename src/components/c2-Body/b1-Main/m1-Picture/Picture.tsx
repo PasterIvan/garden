c8a7ae1ba6e8ac414903f8552ba91b8d87056afc
Ivan <paster-ivan@yandex.by>
2022-08-01 20:10:31 +0300

    fonts test

--- src/components/c2-Body/b1-Main/m1-Picture/Picture.tsx
+++ src/components/c2-Body/b1-Main/m1-Picture/Picture.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import style from "./Picture.module.css"
 import Group333553 from './img/button/Group33553.svg'
+import test from './img/test.svg'
 
 export const Picture = () => {
     return (
         <div className={style.picture}>
             <div className={style.content}>
-                <div className={style.text1}>Старый <div>сад </div> <br/> на новый лад</div>
+                <div className={style.text1}>Старый <div>сад </div> <br/> на новый лад
+                <img src={test}/>
+                </div>
                 <div className={style.text2}>Выбери свое дерево</div>
                 <div className={style.button}>
                     Выбрать дерево
