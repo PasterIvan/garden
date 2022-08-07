0541f46857cab36dfb05c0da6e36fae71e164f4f
Ivan <paster-ivan@yandex.by>
2022-08-08 01:13:23 +0300

    test

--- src/components/c1-Header/h1-Menu/HeaderMenu.tsx
+++ src/components/c1-Header/h1-Menu/HeaderMenu.tsx
@@ -1,5 +1,5 @@
 import {NavLink} from "react-router-dom";
-import React, {useState} from "react";
+import React from "react";
 import Button from "../../../common/Button/Button";
 import style from "./HeaderMenu.module.css"
 import logo from "./img/logo.svg";
